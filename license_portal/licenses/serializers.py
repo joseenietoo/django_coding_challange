f547427a89f5f523ee828929d497c22b569daef3
José Nieto <josee.nietoo@gmail.com>
2023-10-14 14:20:08 -0300

    FEAT: create serializers.py for mapping models from-to JSON

---
+++ license_portal/licenses/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import License
+
+class LicenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = License
+        fields = ['client', 'package', 'license_type', 'created_datetime', 'expiration_datetime']
