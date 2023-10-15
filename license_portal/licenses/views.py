53e1fc63071457eba7c4152a3cb9efaa980fe59b
José Nieto <josee.nietoo@gmail.com>
2023-10-15 16:52:55 -0300

    FEAT: add logic for expiration date license email notification system

--- license_portal/licenses/views.py
+++ license_portal/licenses/views.py
@@ -5,3 +5,31 @@ from .serializers import LicenseSerializer
 class LicenseViewSet(viewsets.ModelViewSet):
     queryset = License.objects.all()
     serializer_class = LicenseSerializer
+
+    def enviar_notificacion_por_correo(self):
+        license = self.get_object()
+
+         if (
+        (license.expiration_datetime - timezone.now()).days == 120
+        or
+        (license.expiration_datetime - timezone.now()).days <= 30 and timezone.now().weekday() == 0
+        or
+        (license.expiration_datetime - timezone.now()).days <= 7
+    ):
+     cuerpo_correo = f"""
+        Información de Licencia:
+
+        ID de Licencia: {license.id}
+        Tipo de Licencia: {license.license_type}
+        Nombre del Paquete: {license.package.get_display_name()}
+        Fecha de Expiración: {license.expiration_datetime}
+        Cliente POC: {license.client.client_poc_name} ({license.client.client_poc_email})
+        """
+
+        send_mail(
+            'Notificación de Expiración de Licencia',
+            cuerpo_correo,
+            'jose.nietov1@mayor.cl',  
+            ['jose.nietov1@mayor.cl'],
+            fail_silently=False,
+        )
