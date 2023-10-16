670346a83ed268c813fa2a3f99cb8004bc20dc0f
José Nieto <josee.nietoo@gmail.com>
2023-10-16 14:46:20 -0300

    FIX: fix name of weekday python attribute of given expiration date

--- license_portal/licenses/views.py
+++ license_portal/licenses/views.py
@@ -1,35 +1,36 @@
-from rest_framework import viewsets
-from .models import License
-from .serializers import LicenseSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.core.mail import send_mail
+from django.utils import timezone
+from .models import License, EmailLog
 
-class LicenseViewSet(viewsets.ModelViewSet):
-    queryset = License.objects.all()
-    serializer_class = LicenseSerializer
+@api_view(['POST'])
+def enviar_notificacion_correo(request):
+    licenses_4_months = License.objects.filter(expiration_datetime__lte=timezone.now() + timezone.timedelta(days=120))
+    licenses_1_month_monday = License.objects.filter(expiration_datetime__lte=timezone.now() + timezone.timedelta(days=30), expiration_datetime__gte=timezone.now(), expiration_datetime__weekday=0)
+    licenses_1_week = License.objects.filter(expiration_datetime__lte=timezone.now() + timezone.timedelta(days=7))
 
-    def enviar_notificacion_por_correo(self):
-        license = self.get_object()
+    licenses = licenses_4_months | licenses_1_month_monday | licenses_1_week
 
-        if (
-            (license.expiration_datetime - timezone.now()).days == 120
-            or
-            (license.expiration_datetime - timezone.now()).days <= 30 and timezone.now().weekday() == 0
-            or
-            (license.expiration_datetime - timezone.now()).days <= 7
-        ):
-            cuerpo_correo = f"""
-                Información de Licencia:
+    for license in licenses:
+        cuerpo_correo = f"""
+            Información de Licencia:
 
-                ID de Licencia: {license.id}
-                Tipo de Licencia: {license.license_type}
-                Nombre del Paquete: {license.package.get_display_name()}
-                Fecha de Expiración: {license.expiration_datetime}
-                Cliente POC: {license.client.client_poc_name} ({license.client.client_poc_email})
-            """
+            ID de Licencia: {license.id}
+            Tipo de Licencia: {license.license_type}
+            Nombre del Paquete: {license.package.get_display_name()}
+            Fecha de Expiración: {license.expiration_datetime}
+            Cliente POC: {license.client.client_poc_name} ({license.client.client_poc_email})
+        """
+        
+        send_mail(
+            'Notificación de Expiración de Licencia',
+            cuerpo_correo,
+            'tu@email.com',
+            ['destinatario@ejemplo.com'],
+            fail_silently=False,
+        )
+        
+        EmailLog.objects.create(license_id=license.id)
 
-            send_mail(
-                'Notificación de Expiración de Licencia',
-                cuerpo_correo,
-                'jose.nietov1@mayor.cl',  
-                ['jose.nietov1@mayor.cl'],
-                fail_silently=False,
-            )
+    return Response({'mensaje': 'Correos enviados exitosamente'})
