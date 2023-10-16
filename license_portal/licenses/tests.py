1157b0be7824aec2256609180e42edb710c6d04e
José Nieto <josee.nietoo@gmail.com>
2023-10-16 19:34:13 -0300

    TEST: add emaillog unit test

--- license_portal/licenses/tests.py
+++ license_portal/licenses/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import EmailLog
 
-# Create your tests here.
+class EmailLogTests(TestCase):
+    def test_creacion_email_log(self):
+        email_log = EmailLog.objects.create(license_id=1)
+        self.assertEqual(email_log.license_id, 1)
