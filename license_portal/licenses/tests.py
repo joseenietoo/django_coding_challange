6515e7e6f76243adf1e83bc36ba30b90b8469301
José Nieto <josee.nietoo@gmail.com>
2023-10-17 03:56:02 -0300

    TEST: update testing

--- license_portal/licenses/tests.py
+++ license_portal/licenses/tests.py
@@ -1,57 +1,50 @@
 from django.test import TestCase
 from licenses.models import License, Client, EmailLog
 
-class LicenseModelTest(TestCase):
-    def setUp(self):
-        self.client = Client.objects.create(
-            client_name="Test Client",
-            poc_contact_name="Test Contact",
-            poc_contact_email="test@example.com",
-            admin_poc=None
+class EnviarNotificacionCorreoTest(TestCase):
+    def test_enviar_notificacion_correo(self):
+        client = Client.objects.create(
+            client_name="Ejemplo Cliente",
+            poc_contact_name="Ejemplo Contacto",
+            poc_contact_email="ejemplo@example.com",
+            admin_poc=1
         )
-        self.license = License.objects.create(
-            client=self.client,
+        license = License.objects.create(
+            client=client,
             package=0,
-            license_type=0
+            license_type=0,
+            expiration_datetime="2023-12-31T23:59:59Z" 
         )
 
-    def test_license_creation(self):
-        self.assertEqual(self.license.client, self.client)
-        self.assertEqual(self.license.package, 0)
-        self.assertEqual(self.license.license_type, 0)
+        response = self.client.post('/api/licenses/enviar_notificacion_correo/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {'mensaje': 'Correos enviados exitosamente'})
 
-class ClientModelTest(TestCase):
-    def setUp(self):
-        self.client = Client.objects.create(
-            client_name="Test Client",
-            poc_contact_name="Test Contact",
-            poc_contact_email="test@example.com",
-            admin_poc=None
+class ListaCorreosEnviadosTest(TestCase):
+    def test_lista_correos_enviados(self):
+        client = Client.objects.create(
+            client_name="Ejemplo Cliente 2",
+            poc_contact_name="Ejemplo Contacto 2",
+            poc_contact_email="ejemplo2@example.com",
+            admin_poc=1
         )
-
-    def test_client_creation(self):
-        self.assertEqual(self.client.client_name, "Test Client")
-        self.assertEqual(self.client.poc_contact_name, "Test Contact")
-        self.assertEqual(self.client.poc_contact_email, "test@example.com")
-
-class EmailLogModelTest(TestCase):
-    def setUp(self):
-        self.client = Client.objects.create(
-            client_name="Test Client",
-            poc_contact_name="Test Contact",
-            poc_contact_email="test@example.com",
-            admin_poc=None
-        )
-        self.license = License.objects.create(
-            client=self.client,
+        license = License.objects.create(
+            client=client,
             package=0,
-            license_type=0
+            license_type=0,
+            expiration_datetime="2023-12-31T23:59:59Z"
         )
-        self.email_log = EmailLog.objects.create(
+        email_log = EmailLog.objects.create(
             sent_at=None,
-            license_id=self.license.id
+            license_id=license.id
         )
 
-    def test_email_log_creation(self):
-        self.assertEqual(self.email_log.license_id, self.license.id)
+        response = self.client.get('/api/licenses/lista_correos_enviados/5/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.data, list)
 
+class ResumenNotificacionesTest(TestCase):
+    def test_resumen_notificaciones(self):
+        response = self.client.get('/api/licenses/resumen_notificaciones/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('total_correos_enviados', response.data)
